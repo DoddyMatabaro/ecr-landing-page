31ec423e056ad911f284febf189dbb3e4d2008b4
Doddy MATABARO <matabarododdy@gmail.com>
2022-09-27 11:17:16 +0200

    Styled Projects components

--- src/components/Dimension.jsx
+++ src/components/Dimension.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import 
 
 function Dimension() {
   return (
-    <div className='h-screen'>Dimension</div>
+    <section className='h-screen flex flex-col justify-center py-10 px-10 align-middle gap-4'>
+        <article className="flex flex-row gap-4">
+                    <div className='bg-black h-4 w-3 mt-5'></div>
+                    <h1 className="text-5xl font-bold ">Inegale dans toutes les dimensions</h1>
+        </article>
+        <article>
+              <div>
+                  <div><img src='oo' alt='ok'/></div>  
+                  <div>Projet</div>  
+              </div>
+        </article>
+    </section>
   )
 }
 
--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import presentation from '../assets/inspiration/presentation.mp4';
 
 function Presentation() {
   return (
-    <div className='h-screen'>Presentation</div>
+   
+      <iframe class="w-full aspect-video md:aspect-square" src={presentation}></iframe>
+
   )
 }
 
